ae9bcbf58b1c4a1e45ac62f212521ad6d5181869
PSY <104129606+kunjappan-PSY@users.noreply.github.com>
2022-04-24 17:06:08 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,13 +48,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @kunjappansupportez</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : ᴋᴜɴᴊᴀᴘᴘᴀɴ ғɪʟᴛᴇʀ ʙᴏᴛ</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @kunjappansupportez    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
