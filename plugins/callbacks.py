9527b962a59af081c2902f427de72d72a4606e8e
PSY <104129606+kunjappan-PSY@users.noreply.github.com>
2022-04-24 17:17:13 +0530

    Update callbacks.py

--- plugins/callbacks.py
+++ plugins/callbacks.py
@@ -22,7 +22,7 @@ from database.connections_mdb import(
 )
 
 
-@trojanz.on_callback_query()
+@kunjappan.on_callback_query()
 async def cb_handler(client, query):
 
     if query.data == "start_data":
@@ -47,12 +47,12 @@ async def cb_handler(client, query):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/hkmc3e7U7R4"),
+                    InlineKeyboardButton("How to Deploy?", url="https://youtu.be/vkkZs6t_DZo"),
                     InlineKeyboardButton("About Me", callback_data="about_data")
                 ],
                 [
-                    InlineKeyboardButton("BOT Channel", url="https://t.me/TroJanzHEX"),
-                    InlineKeyboardButton("Support Group", url="https://t.me/TroJanzSupport")
+                    InlineKeyboardButton("BOT Channel", url="https://t.me/kunjappansupportez"),
+                    InlineKeyboardButton("Support Group", url="https://t.me/kunjappansupport")
                 ]
             ]
         )
@@ -70,7 +70,7 @@ async def cb_handler(client, query):
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Unlimited-Filter-Bot")
+                        "SOURCE CODE", url="https://github.com/kunjappan-PSY/justice")
                 ],
                 [
                     InlineKeyboardButton("BACK", callback_data="help_data"),
